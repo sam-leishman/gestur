3e04b171f4d42d6a77142218020324124c71a7ff
samleishman <samuel.leishman@sparkz.systems>
2026-03-13 15:25:34 -0600

    Add route protection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,16 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+const PUBLIC_ROUTES = [
+	'/login',
+	'/register'
+];
+const AUTH_ROUTES = [
+	'/login',
+	'/register'
+];
 
 const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	const session = await auth.api.getSession({ headers: event.request.headers });
@@ -9,6 +18,18 @@ const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	if (session) {
 		event.locals.session = session.session;
 		event.locals.user = session.user;
+	}
+
+	// Route protection
+	const isPublicRoute = PUBLIC_ROUTES.includes(event.url.pathname) || event.url.pathname.startsWith('/api/auth/');
+	const isAuthRoute = AUTH_ROUTES.includes(event.url.pathname);
+
+	if (isAuthRoute && session?.user) {
+		throw redirect(303, '/');
+	}
+
+	if (!isPublicRoute && !session?.user) {
+		throw redirect(303, '/login');
 	}
 
 	return svelteKitHandler({ event, resolve, auth, building });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
     import { authClient } from "$lib/auth-client";
     
     const session = authClient.useSession();
+
+    async function handleSignOut() {
+        await authClient.signOut();
+        goto('/login');
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 {#if $session.data}
     <p>Logged in as {$session.data.user.name}</p>
-    <button onclick={() => authClient.signOut()} class="btn btn-primary">Sign out</button>
+    <button onclick={handleSignOut} class="btn btn-primary">Sign out</button>
 {/if}
